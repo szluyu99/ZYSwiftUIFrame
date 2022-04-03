c470fe08e3bf11e96e9e5ebdcbcc50f7c4d6ed62
yusael <yusael@163.com>
2022-04-03 22:19:05 +0800

    test

--- go_api_server/main.go
+++ go_api_server/main.go
@@ -23,12 +23,6 @@ func main() {
 	r := gin.Default()
 	r = router.CollectionRoutes(r) // 注册路由
 
-	// 检测健康
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "ping",
-		})
-	})
 	r.Run() // 监听并在 0.0.0.0:8080 上启动服务
 }
 
